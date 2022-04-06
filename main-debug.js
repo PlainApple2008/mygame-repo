af75f6a1e6b770bad042866af2de5dd4d417ce45
danielderrickmcalisura@gmail.com <danielderrickmcalisura@gmail.com>
2022-04-06 16:34:51 +0800

    modified 3 files

--- main-debug.js
+++ main-debug.js
@@ -1,3 +1,7 @@
 console.log('main-debug.js start');
 
+function focus(content) {
+  document.getElementById('debug-focusedcontent').innerHTML = content;
+}
+
 console.log('main-debug.js end');
--- scripts/gamethread.js
+++ scripts/gamethread.js
@@ -1,3 +1,10 @@
 console.log('gamethread.js start');
 
+function run() {
+  focus('function run called');
+  requestAnimationFrame(run);
+}
+
+run();
 console.log('gamethread.js end');
+
